0212b3ba9a318b33cfbf6fbd00dd8e913fe72298
harshyelwatkar <128224198+harshyelwatkar@users.noreply.github.com>
2025-03-17 03:18:50 +0530

    Delete src directory

--- src/main/java/com/blog/repositories/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.blog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.blog.entities.Category;
-
-public interface CategoryRepo extends JpaRepository<Category, Integer>{
-
-}
